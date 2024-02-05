2825a384537a1b4200f386fc2fead0ccc09e072c
Mateus Lino <mateusantos05@gmail.com>
2024-02-05 17:44:48 -0300

    Add InMemory preference type

---
+++ Sources/Preferences/InMemory.swift
@@ -0,0 +1,57 @@
+// Created by Mateus Lino
+
+import Foundation
+
+@propertyWrapper
+public struct InMemory<Value: PersistenceValue, Preferences: PreferencesProtocol> {
+    let defaultValue: Value?
+
+    public var wrappedValue: Value? {
+        get {
+            fatalError("Wrapped value should not be used")
+        }
+        set {
+            fatalError("Wrapped value should not be used")
+        }
+    }
+
+    public init(wrappedValue: Value? = nil) {
+        self.defaultValue = wrappedValue
+    }
+
+    public static subscript(
+        _enclosingInstance instance: Preferences,
+        wrapped wrappedKeyPath: ReferenceWritableKeyPath<Preferences, Value?>,
+        storage storageKeyPath: ReferenceWritableKeyPath<Preferences, Self>
+    ) -> Value? {
+        get {
+            return value(_enclosingInstance: instance, storage: storageKeyPath)
+        }
+        set {
+            guard value(_enclosingInstance: instance, storage: storageKeyPath) != newValue else {
+                return
+            }
+
+            do {
+                let encoded = try JSONEncoder().encode(newValue)
+                instance.inMemoryDataStore[storageKeyPath] = encoded
+                instance.preferencesChangedSubject.send(wrappedKeyPath)
+            } catch {
+                print(error)
+            }
+        }
+    }
+
+    private static func value(
+        _enclosingInstance instance: Preferences,
+        storage storageKeyPath: ReferenceWritableKeyPath<Preferences, Self>
+    ) -> Value? {
+        let defaultValue = instance[keyPath: storageKeyPath].defaultValue
+
+        guard let data = instance.inMemoryDataStore[storageKeyPath] else {
+            return defaultValue
+        }
+
+        return try? JSONDecoder().decode(Value.self, from: data)
+    }
+}
