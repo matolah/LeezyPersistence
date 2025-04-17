61a9b1cbb1a47b2b0eded7b5dc682daf333bec80
Mateus Lino <26446518+matolah@users.noreply.github.com>
2025-04-17 10:03:22 -0300

    Add property observer tests

---
+++ Tests/PreferenceTests.swift
@@ -0,0 +1,52 @@
+import Combine
+import XCTest
+
+@testable import LeezyPersistence
+
+final class PreferenceTests: XCTestCase {
+    private class InMemoryMockPreferences: MockPreferences {
+        @InMemory<String, InMemoryMockPreferences> var testKey: String? {
+            didSet {
+                updatedProperty = "updated"
+            }
+        }
+
+        var updatedProperty = ""
+
+        var testKeyStorageKeyPath: ReferenceWritableKeyPath<InMemoryMockPreferences, InMemory<String, InMemoryMockPreferences>> {
+            \InMemoryMockPreferences._testKey
+        }
+    }
+
+    private class MockViewModel {
+        @Preference(\InMemoryMockPreferences.testKey) var testKey
+    }
+
+    private var mockPreferences: InMemoryMockPreferences!
+
+    private var updatedProperty = ""
+
+    @Preference(\InMemoryMockPreferences.testKey) var testKey {
+        didSet {
+            updatedProperty = "updated"
+        }
+    }
+
+    override func setUp() {
+        super.setUp()
+        mockPreferences = InMemoryMockPreferences()
+    }
+
+    override func tearDown() {
+        mockPreferences = nil
+        super.tearDown()
+    }
+
+    func testPreferencePropertyObserver() {
+        testKey = "TestValue"
+
+        let expectedValue = "updated"
+        XCTAssertEqual(updatedProperty, expectedValue)
+        XCTAssertEqual(mockPreferences.updatedProperty, expectedValue)
+    }
+}
